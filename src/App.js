3c399b401223b022bc5fb95925f28cea6bcc1d28
Elena-Marchenko <73835886+Elena-Marchenko@users.noreply.github.com>
2021-07-02 22:02:43 +0300

    заканчивает дз1

--- src/App.js
+++ src/App.js
@@ -1,16 +1,10 @@
-import Profile from './components/social-profile/Profile';
-import profiles from './user.json';
+import ProfileList from './components/social-profile/ProfileList';
+import profiles from './components/social-profile/user.json';
 
 export default function App() {
   return (
-    <Profile
-      name={profiles[0].name}
-      tag={profiles[0].tag}
-      location={profiles[0].location}
-      avatar={profiles[0].avatar}
-      followers={profiles[0].stats.followers}
-      views={profiles[0].stats.views}
-      likes={profiles[0].stats.likes}
-    />
+    <div>
+      <ProfileList items={profiles} />
+    </div>
   );
 }
---
+++ src/components/social-profile/ProfileList.js
@@ -0,0 +1,30 @@
+import Profile from './Profile';
+import PropTypes from 'prop-types';
+
+export default function ProfileList({ items }) {
+  return (
+    <ul>
+      {items.map(item => (
+        <li key={item.name}>
+          <Profile
+            name={item.name}
+            tag={item.tag}
+            location={item.location}
+            avatar={item.avatar}
+            followers={item.stats.followers}
+            views={item.stats.views}
+            likes={item.stats.likes}
+          />
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+ProfileList.propTypes = {
+  items: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+    }),
+  ),
+};
