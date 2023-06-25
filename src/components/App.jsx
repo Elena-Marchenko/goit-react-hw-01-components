96868b81ae1d0b476ecd621e91e2544cff3af6df
Elena-Marchenko <73835886+Elena-Marchenko@users.noreply.github.com>
2023-06-25 16:23:43 +0300

    edit
    add statistics

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,12 @@
 // import { Profile } from 'components/profile/Profile';
 // import { Statistics } from 'components/statistics/Statistics';
+import { Statistics } from 'components/statistics/Statistics';
 import { FriendList } from 'components/friendsList/FriendList';
 import { TransactionHistory } from 'components/transactionHistory/TransactionHistory';
 
 // import userData from 'data/user.json';
 // import statsData from 'data/data.json';
+import statsData from 'data/data.json';
 import friendsData from 'data/friends.json';
 import transactions from 'data/transactions.json';
 
@@ -25,6 +27,11 @@ export const App = () => {
         <Statistics title="Upload stats" stats={statsData} />
         <Statistics stats={statsData} />
       </div> */}
+      <div>
+        <Statistics title="Upload stats" stats={statsData} />
+        {/* <Statistics stats={statsData} /> */}
+      </div>
+
       <div>
         <FriendList friends={friendsData} />
       </div>
