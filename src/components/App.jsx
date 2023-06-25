8da439111b572ee63f2b5a8d39cafd7834a6e6d1
Elena-Marchenko <73835886+Elena-Marchenko@users.noreply.github.com>
2023-06-25 15:27:18 +0300

    edit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,10 @@
 // import { Profile } from 'components/profile/Profile';
-import { Statistics } from 'components/statistics/Statistics';
+// import { Statistics } from 'components/statistics/Statistics';
 import { FriendList } from 'components/friendsList/FriendList';
 import { TransactionHistory } from 'components/transactionHistory/TransactionHistory';
 
 // import userData from 'data/user.json';
-import statsData from 'data/data.json';
+// import statsData from 'data/data.json';
 import friendsData from 'data/friends.json';
 import transactions from 'data/transactions.json';
 
@@ -21,10 +21,10 @@ export const App = () => {
         />
       </div> */}
 
-      <div>
+      {/* <div>
         <Statistics title="Upload stats" stats={statsData} />
-        {/* <Statistics stats={statsData} /> */}
-      </div>
+        <Statistics stats={statsData} />
+      </div> */}
       <div>
         <FriendList friends={friendsData} />
       </div>
